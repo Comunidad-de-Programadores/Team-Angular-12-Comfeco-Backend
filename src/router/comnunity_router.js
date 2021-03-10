f3792aef8b201b744023cc34d9902584183db2f9
000Erick <erick.diaz3@unmsm.edu.pe>
2021-03-10 15:35:22 -0500

    fix get community router and creator router

--- src/router/comnunity_router.js
+++ src/router/comnunity_router.js
@@ -9,7 +9,7 @@ const mdAutenticacion = require('../middlewares/autenticacion');
 
 //Obtener toda las comunidades
 
-app.get('', mdAutenticacion, async(req, res) => {
+app.get('', async(req, res) => {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 5;
     const desde = (page - 1) * limit;
